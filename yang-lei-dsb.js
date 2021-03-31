069e8f779f6ca0850308a44337a8595918d78615
码梦天涯 <jerry4718@qq.com>
2021-03-31 15:04:16 +0800

    Update yang-lei-dsb.js

--- yang-lei-dsb.js
+++ yang-lei-dsb.js
@@ -1,7 +1,7 @@
 addEventListener("fetch", (event) => { 
   event.respondWith( 
     new Response(
-      "你懂我的意思吧 [yang-lei-dsb]", 
+      "Hello Deno Deploy [yang-lei-dsb]", 
       { 
         status: 200, 
         headers: { server: "denosr", "content-type": "text/plain", }, 
