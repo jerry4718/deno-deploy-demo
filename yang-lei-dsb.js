861d8e860f6dc883e83101691f17bac7806258a6
码梦天涯 <jerry4718@qq.com>
2021-03-31 15:11:02 +0800

    Update yang-lei-dsb.js

--- yang-lei-dsb.js
+++ yang-lei-dsb.js
@@ -2,7 +2,7 @@ addEventListener("fetch", (event) => {
   const response = new Response(
     new TextEncoder().encode("你懂我的意思吧 [yang-lei-dsb]"),
     {
-      headers: { "content-type": "text/plain" },
+      headers: { "content-type": "text/plain; charset=utf-8" },
     },
   );
   event.respondWith(response);
